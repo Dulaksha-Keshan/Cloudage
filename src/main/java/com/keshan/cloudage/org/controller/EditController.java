cf0e59014d59b37ebe13a83df168bc7938a3cc7f
Dulaksha-Keshan <dulakshakesh@gmail.com>
2025-09-02 13:54:36 +0530

    Exception handling in other controller 1.1

--- src/main/java/com/keshan/cloudage/org/controller/EditController.java
+++ src/main/java/com/keshan/cloudage/org/controller/EditController.java
@@ -5,40 +5,40 @@ import com.keshan.cloudage.org.common.CustomException;
 import com.keshan.cloudage.org.model.enums.CustomExceptionCode;
 import com.keshan.cloudage.org.model.enums.ITYPE;
 import com.keshan.cloudage.org.service.EditService;
-import lombok.AllArgsConstructor;
+import jakarta.validation.constraints.NotBlank;
 import lombok.RequiredArgsConstructor;
-import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
+import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.*;
 
 import java.io.IOException;
 import java.io.InputStream;
 import java.util.Arrays;
-import java.util.List;
+import java.util.Set;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
 
 @RestController
 @RequestMapping("api/edit")
 @RequiredArgsConstructor
-@CrossOrigin(origins = "http://localhost:5173/")
 public class EditController {
 
     private final EditService editService;
+    private final static Set<String> ALLOWED_LIST = Stream.concat(Arrays.stream(ITYPE.values()).map(ITYPE::getFormat),
+            Arrays.stream(ITYPE.values()).map(ITYPE::getMIME)). collect(Collectors.toSet());
 
 //TODO later maybe try to use dto for this controller (most likely for request only )
-//TODO save the edited image to the s3 and in the DB
+//TODO save the edited image to the s3 and in the DB also try to get these(Controller logics) logics under the edit service
     @GetMapping("formatChange")
     public ResponseEntity<?> formatChange(
             @RequestParam String s3key,
+            @NotBlank
             @RequestParam String format
     ) throws IOException {
-
-        List<String> allowedTypes = Arrays.stream(ITYPE.values()).map(ITYPE::getFormat).toList();
-        if (!allowedTypes.contains(format.toLowerCase())) {
-            return ResponseEntity.badRequest().body("Invalid format type");
-        }else{
+            checkType(format);
             format = ITYPE.fromFormat(format.toLowerCase()).getFormat();
-        }
+
             InputStream image = editService.retrieveImageAsInputStream(s3key);
 
             byte[] imageBytes = editService.imageConversionToFormat(image,format);
@@ -59,20 +59,16 @@ public class EditController {
                 return ResponseEntity.ok(editService.imageConversionToASCII(imageBytes));
 
             }else {
-
-                List<String> allowedTypes = Arrays.stream(ITYPE.values()).map(ITYPE::getMIME).toList();
-                if (!typeValid(allowedTypes,MIME)) {
-                    throw new CustomException(CustomExceptionCode.INVALID_MIME_TYPE,MIME);
-                }
-
+                checkType(MIME);
                 return ResponseEntity.ok(editService.imageConversionToASCII(imageBytes,MIME));
             }
 
     }
 
-    private boolean typeValid(List<String> allowedList,String type){
-
-        return allowedList.contains(type.strip().toLowerCase());
+    private  void checkType(String type){
+        if(!ALLOWED_LIST.contains(type.strip().toLowerCase())){
+            throw new CustomException(CustomExceptionCode.INVALID_MIME_TYPE,type);
+        }
     }
 
 }
